3d9d04a9ae4d09b735f327099eb23fc602c69810
chenmortal <2584139809@qq.com>
2026-08-01 23:21:09 +0800

    fix(test): fix frontend e2e

--- tests/configEditor.spec.ts
+++ tests/configEditor.spec.ts
@@ -1,31 +1,18 @@
 import { test, expect } from '@grafana/plugin-e2e';
-import { MyDataSourceOptions, MySecureJsonData } from '../src/types';
 
 test('smoke: should render config editor', async ({ createDataSourceConfigPage, readProvisionedDataSource, page }) => {
   const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
   await createDataSourceConfigPage({ type: ds.type });
-  await expect(page.getByLabel('Path')).toBeVisible();
-});
-test('"Save & test" should be successful when configuration is valid', async ({
-  createDataSourceConfigPage,
-  readProvisionedDataSource,
-  page,
-}) => {
-  const ds = await readProvisionedDataSource<MyDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
-  const configPage = await createDataSourceConfigPage({ type: ds.type });
-  await page.getByRole('textbox', { name: 'Path' }).fill(ds.jsonData.path ?? '');
-  await page.getByRole('textbox', { name: 'API Key' }).fill(ds.secureJsonData?.apiKey ?? '');
-  await expect(configPage.saveAndTest()).toBeOK();
+  await expect(page.getByRole('textbox', { name: 'Data source connection URL' })).toBeVisible();
+  await expect(page.getByText('Interface Version', { exact: false })).toBeVisible();
 });
 
-test('"Save & test" should fail when configuration is invalid', async ({
+test('"Save & test" should fail when no valid URL is configured', async ({
   createDataSourceConfigPage,
   readProvisionedDataSource,
-  page,
 }) => {
-  const ds = await readProvisionedDataSource<MyDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
+  const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
   const configPage = await createDataSourceConfigPage({ type: ds.type });
-  await page.getByRole('textbox', { name: 'Path' }).fill(ds.jsonData.path ?? '');
   await expect(configPage.saveAndTest()).not.toBeOK();
-  await expect(configPage).toHaveAlert('error', { hasText: 'API key is missing' });
+  await expect(configPage).toHaveAlert('error');
 });
